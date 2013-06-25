070ace55101cda493b2e08be73a6cdc221545098
josh-willhite <jwillhite@gmail.com>
2013-06-24 19:50:28 -0700

    dxf reader will extract geometry information into line objects which are returned as a list

---
+++ DXFReader.py
@@ -0,0 +1,41 @@
+'''
+Created on Jun 24, 2013
+
+@author: Josh Willhite
+'''
+
+class Line:
+    def __init__(self, layer, x0, y0, z0, x1, y1, z1): 
+        self.x0 = x0        #starting points
+        self.y0 = y0
+        self.z0 = z0
+        self.x1 = x1        #end points
+        self.y1 = y1
+        self.z1 = z1
+        self.layer = layer    
+       
+    def __str__(self):
+        return "\nLayer:%s (%.3f, %.3f, %.3f) (%.3f, %.3f, %.3f)\n" % (self.layer, self.x0, self.y0, 
+                                                                                 self.z0, self.x1, self.y1, self.z1)
+    
+def getLines(fileName):
+    lines = []  #array to hold line objects
+
+    f = open(fileName, 'r') 
+    dxf = f.read()  
+    f.close()
+    
+    geoTextBlock = dxf[dxf.index('ENTITIES') + 8:dxf.index('VIEWPORT')].split('  0') #filter the geometry objects into a list
+      
+    geoTextBlock = geoTextBlock[1:len(geoTextBlock) - 1] #remove the 1st and last empty elements of the list
+    
+    for textBlock in geoTextBlock:
+        shape = textBlock.split('\n')
+        if shape[1] == 'LINE':
+            currLine = Line(shape[5], float(shape[7]), float(shape[9]), float(shape[11]), float(shape[13]), float(shape[15]), float(shape[17]))
+            lines.append(currLine)
+    return lines
+    
+
+lines = getLines("test.dxf")
+
