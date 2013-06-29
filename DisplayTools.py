ac0b1174efe48bc8aaeda9c05c7528aae361629f
josh-willhite <jwillhite@gmail.com>
2013-06-29 12:54:20 -0700

    Added MIT License

--- DisplayTools.py
+++ DisplayTools.py
@@ -1,5 +1,5 @@
 '''
-Created on Jun 25, 2013
+Created on Jun 29, 2013
 
 Copyright (c) 2013 @author: Josh Willhite
 
@@ -21,29 +21,5 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
-from tkinter import *
-#from tkinter import ttk
-import DXFReader
-
-lines = DXFReader.getLines("test.dxf")
-
-for line in lines:
-    print(line)
-
-root = Tk()
-root.title("PANEL VIEWER")
 
 
-#frame = ttk.Frame(root)
-
-c = Canvas(root, height=800, width=800, bg="black")
-
-for line in lines:
-    if line.layer == "YELLOW":
-        c.create_line(line.x0, line.y0, line.x1, line.y1, fill=line.layer, dash=(3,5))
-    else:
-        c.create_line(line.x0, line.y0, line.x1, line.y1, fill=line.layer)
-
-
-c.pack()
-root.mainloop()
