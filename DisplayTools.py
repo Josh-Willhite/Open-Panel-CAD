cfd5d23362dfd5faeb69bdceda9e0e0d4ffc2345
josh-willhite <jwillhite@gmail.com>
2013-07-22 20:56:47 -0700

    small refactor

--- DisplayTools.py
+++ DisplayTools.py
@@ -10,6 +10,9 @@ import math
 
 def scaleAndCenter(panel, canvasWidth, canvasHeight, size):
     #assumes panel is always going to be much smaller then canvas, check this later
+    assert panel.maxWidth() < canvasWidth , "panel is larger then canvas"
+    assert panel.maxHeight() < canvasHeight, "panel is larger then canvas"
+
     xDelta = math.fabs(panel.maxWidth() - canvasWidth)
     yDelta = math.fabs(panel.maxHeight() - canvasHeight)
 
@@ -23,6 +26,26 @@ def scaleAndCenter(panel, canvasWidth, canvasHeight, size):
 
     return [scale, xTranslate, yTranslate]
 
+def center3D(panel, width, height):
+    if panel.xMin() == 0:
+        xTranslate = -(panel.maxWidth()/2)
+
+    if panel.xMin() > 0:
+        xTranslate = -(panel.xMin() + panel.maxWidth()/2)
+
+    if panel.xMin() < 0:
+        xTranslate = panel.xMin() - panel.maxWidth()/2
+
+    if panel.yMin() == 0:
+        yTranslate = -(panel.maxHeight()/2)
+
+    if panel.yMin() > 0:
+        yTranslate = -(panel.yMin() + panel.maxHeight()/2)
+
+    if panel.yMin() < 0:
+        yTranslate = panel.yMin() - panel.maxHeight()/2
+
+    return [xTranslate, yTranslate]
 
 # add tools for navigating "model space"
 
